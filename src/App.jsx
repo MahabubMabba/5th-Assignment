61792d03e505a23e5411913aee74c25b84b4f712
MahabubMabba <mahabubhasab0161587@gmail.com>
2026-09-16 22:36:57 +0600

    add nav

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
+import Nav from './components/Nav'
 
 function App() {
   
 
   return (
     <>
+     <Nav></Nav>
      
     </>
   )
---
+++ src/components/Nav.tsx
@@ -0,0 +1,29 @@
+import NavLogo from "../assets/logo-text.png";
+
+const Nav = () => {
+  return (
+    <div className=" container mx-auto  items-center py-4">
+      <nav className="flex justify-between items-center ">
+        <div>
+          <img src={NavLogo} alt="" />
+        </div>
+        <div className="flex gap-6">
+          <a href="" className="text-pink-600">Home</a>
+          <a href="">Technologies</a>
+          <a href="">Projects</a>
+          <a href="">About</a>
+          <a href="">Contact</a>
+        </div>
+        <div className="flex gap-1">
+          <button className="btn bg-white text-black rounded-2xl">Sign in</button>
+          <button className="btn  bg-pink-700 border-none font-semibold rounded-2xl">Sign Up</button>
+        </div>
+        
+      </nav>
+      <div className="divider"></div>
+    </div>
+    
+  );
+};
+
+export default Nav;
